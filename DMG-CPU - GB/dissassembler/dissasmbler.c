52769ae357f6374fcd8f87361a4115328b1803bb
jon-bassi <jon-bassi@users.noreply.github.com>
2024-04-26 17:17:13 -0700

    test dissassem

--- DMG-CPU - GB/dissassembler/dissasmbler.c
+++ DMG-CPU - GB/dissassembler/dissasmbler.c
@@ -17,9 +17,24 @@ int main(int argc, void** argv)
         return 1;
     }
 
-    uint8_t* bin = dw_fread(filename);
+    uint8_t* bin = NULL;
+    long binLen = dw_fread(filename, &bin);
 
-    printf("%x%x%x%x\n",bin[0],bin[1],bin[2],bin[3]);
+    // printf("%x%x%x%x\n",bin[0],bin[1],bin[2],bin[3]);
+
+    int i = 0;
+    while (i < binLen)
+    {
+        uint8_t opcode = bin[i];
+        OPCODE curOpcode = opcodes[opcode];
+        printf("%04x: %s\n",i,curOpcode.name);
+        i = i + curOpcode.len;
+        if (i > 200)
+        {
+            break;
+        }
+    }
+
 
     free(bin);
 
