bc58d3f8398942279448f9938e85708d6ae322f7
jon-bassi <jon-bassi@users.noreply.github.com>
2024-04-26 20:59:44 -0700

    extra info in prints

--- DMG-CPU - GB/dissassembler/dissasmbler.c
+++ DMG-CPU - GB/dissassembler/dissasmbler.c
@@ -22,17 +22,43 @@ int main(int argc, void** argv)
 
     // printf("%x%x%x%x\n",bin[0],bin[1],bin[2],bin[3]);
 
-    int i = 0;
-    while (i < binLen)
+    
+
+
+    int pc = 0;
+    while (pc < binLen)
     {
-        uint8_t opcode = bin[i];
+        uint8_t opcode = bin[pc];
         OPCODE curOpcode = opcodes[opcode];
-        printf("%04x: %s\n",i,curOpcode.name);
-        i = i + curOpcode.len;
-        if (i > 200)
+
+        char instBytes[9];
+
+        if (curOpcode.len > 1)
         {
-            break;
+            char opcodeStr[24];
+            uint16_t opcodeData = 0x0;
+
+            if (curOpcode.len == 2)
+            {
+                opcodeData = bin[pc+1];
+                snprintf(instBytes,sizeof(instBytes),"%02x %02x   ",bin[pc],bin[pc+1]);
+            }
+            else if (curOpcode.len == 3)
+            {
+                opcodeData = (bin[pc+2] << 8) | bin[pc+1];
+                snprintf(instBytes,sizeof(instBytes),"%02x %02x %02x",bin[pc],bin[pc+1],bin[pc+2]);
+            }
+
+            snprintf(opcodeStr,sizeof(opcodeStr),curOpcode.name,opcodeData);
+            printf("%04x: %s %s\n",pc,instBytes,opcodeStr);
         }
+        else
+        {
+            snprintf(instBytes,sizeof(instBytes),"%02x      ",bin[pc]);
+            printf("%04x: %s %s\n",pc,instBytes,curOpcode.name);
+        }
+        
+        pc = pc + curOpcode.len;
     }
 
 
